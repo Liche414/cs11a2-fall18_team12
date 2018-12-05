fa529519974927a929429d88ee3f6b46632a49ba
Farquhar-Reese <Reese>
2018-12-04 21:19:17 -0500

    Max and Min
    I added the max and min if statements

--- QuizMaker.java
+++ QuizMaker.java
@@ -2,29 +2,29 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class QuizMaker{
-    public static void main(String[] args){
-      //Variables used for this quiz, feel free to change them to suit the quiz you want
-      int questions = 3;    //sets number of questions
-      int minOp = 1;        //sets minimum operand
-      int maxOp = 10;       //sets max operand
-      int go = 0;           //handles menu entries
-      String ops = "+";     //dictates what operation is performed in the quiz
-      int ran1 = 0;         //first random number in the quiz, it can be any number between the min and max operands
-      int ran2 = 0;         //second random number in the quiz, it can be any number between the min and max operands unless the operation is division then it can never be zero
-      int ans = 0;          //answer calculated by the program this is never a decimal
-      double ans2 = 0;      //answer calculated by the program specifically for division
-      int useans = 0;       //answer the user gives
-      double useans2 = 0;   //answer the user gives specific to the division problem
-      int correct = 0;      //counts the number of correct answers the user gives
-      int theruns = 0;      //counts the number of questions took to show how many of the questions were answered correctly out of the total number
+  public static void main(String[] args){
+    //Variables used for this quiz, feel free to change them to suit the quiz you want
+    int questions = 3;    //sets number of questions
+    int minOp = 1;        //sets minimum operand
+    int maxOp = 10;       //sets max operand
+    int go = 0;           //handles menu entries
+    String ops = "+";     //dictates what operation is performed in the quiz
+    int ran1 = 0;         //first random number in the quiz, it can be any number between the min and max operands
+    int ran2 = 0;         //second random number in the quiz, it can be any number between the min and max operands unless the operation is division then it can never be zero
+    int ans = 0;          //answer calculated by the program this is never a decimal
+    double ans2 = 0;      //answer calculated by the program specifically for division
+    int useans = 0;       //answer the user gives
+    double useans2 = 0;   //answer the user gives specific to the division problem
+    int correct = 0;      //counts the number of correct answers the user gives
+    int theruns = 0;      //counts the number of questions took to show how many of the questions were answered correctly out of the total number
 
-      Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-      Random rand = new Random(); // Declare a Random object, used for generating random numbers
+    Random rand = new Random(); // Declare a Random object, used for generating random numbers
 
-      System.out.println("Hello User!");
+    System.out.println("Hello User!");
 
-      while(go==0){
+    while(go==0){
 
       //Quiz Settings
       System.out.println("");
@@ -64,14 +64,55 @@ public class QuizMaker{
           go--;
         }
         //do loop checks for input errors
-            do{
-                while(!sc.hasNextInt()){
-                    sc.next();
-                    System.out.println("Error: Number of questions " + "must be a positive integer. ");
-                  }
-                  questions=sc.nextInt();
-                  if(questions<1){
-                    System.out.println("Error: Number of questions " + "must be a positive integer. ");
-                  }
-                } while(questions<1);
-              }
+        do{
+          while(!sc.hasNextInt()){
+            sc.next();
+            System.out.println("Error: Number of questions " + "must be a positive integer. ");
+          }
+          questions=sc.nextInt();
+          if(questions<1){
+            System.out.println("Error: Number of questions " + "must be a positive integer. ");
+          }
+        } while(questions<1);
+      }
+      //If user choses to set minimum operand value
+      if(go==2){
+
+        System.out.println("Enter a value for the minimum " +
+        "possible operand.");
+        if(maxOp>minOp){
+          go=0;
+        }
+        //do loop checks for input errors
+        do{
+          while(!sc.hasNextInt()){
+            sc.next();
+            System.out.println("Error: Minimum operand must be " +
+            "an integer.");
+          }
+          minOp=sc.nextInt();
+          if(maxOp<minOp){
+            System.out.println("Error: The minimum operand must be " +
+            "strictly less than the maximum operand.");
+          }
+        }while(maxOp<minOp);
+      }
+
+      //If user choses to set maximum operand value
+      if(go==3){
+        System.out.println("Enter a value for the maximum operand");
+        if(maxOp>minOp){
+          go=0;
+        }
+        //do loop checks for inputs errors
+        do{
+          while(!sc.hasNextInt()){
+            sc.next();
+            System.out.println("Error: Maximum operand must be an " + "integer.");
+          }
+          maxOp=sc.nextInt();
+          if(maxOp<minOp){
+            System.out.println("Error: The maximum operand " + "must be " + "strictly greater than the minimum operand.");
+          }
+        }while(maxOp<minOp);
+      }
