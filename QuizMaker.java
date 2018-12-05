f9a34cc6479d420ce783cfafb3a8b03e327df485
Farquhar-Reese <Reese>
2018-12-04 22:40:03 -0500

    Formatted Correction
    Fixed Indentation

--- QuizMaker.java
+++ QuizMaker.java
@@ -117,152 +117,152 @@ public class QuizMaker{
         }while(maxOp<minOp);
       }
       //If user choses to set operator
-            if(go==4){
+      if(go==4){
 
-                System.out.println("Select an operator: (+,-,*,/,%)");
-                //do loop checks for input errors
-                do{
-                    ops=sc.next();
-                    if(ops.equals("+") || ops.equals("-") || ops.equals("*") || ops.equals("/") || ops.equals("%")){
-                        go=0;
-                    }
+        System.out.println("Select an operator: (+,-,*,/,%)");
+        //do loop checks for input errors
+        do{
+          ops=sc.next();
+          if(ops.equals("+") || ops.equals("-") || ops.equals("*") || ops.equals("/") || ops.equals("%")){
+            go=0;
+          }
 
-                    if(!ops.equals("+") && !ops.equals("-") && !ops.equals("*") && !ops.equals("/") && !ops.equals("%")){
-                        System.out.println("Error: Invalid operator choice.");
-                        ops=sc.next();
-                    }
+          if(!ops.equals("+") && !ops.equals("-") && !ops.equals("*") && !ops.equals("/") && !ops.equals("%")){
+            System.out.println("Error: Invalid operator choice.");
+            ops=sc.next();
+          }
 
-                } while(!ops.equals("+") && !ops.equals("-") && !ops.equals("*") && !ops.equals("/") && !ops.equals("%"));
-                go=0;
+        } while(!ops.equals("+") && !ops.equals("-") && !ops.equals("*") && !ops.equals("/") && !ops.equals("%"));
+        go=0;
 
+      }
+
+      if(go==5){
+        theruns=0;
+        correct=0;
+        while(theruns<questions){
+          ran1=rand.nextInt(maxOp+1-minOp) + minOp;
+          ran2=rand.nextInt(maxOp+1-minOp) + minOp;
+          //this is the code that handles the addition questions
+          if(ops.equals("+")){
+            System.out.println(ran1 + " + " + ran2);
+
+            ans = ran1 + ran2;
+
+            do{
+              while(!sc.hasNextInt()){
+                sc.next();
+                System.out.println("Error: Invalid answer.");
+
+              }
+            } while(!sc.hasNextInt());
+            useans=sc.nextInt();
+            if(useans==ans){
+              System.out.println("Correct!");
+              correct++;
+            }
+            else{
+              System.out.println("Incorrect");
             }
 
-            if(go==5){
-                theruns=0;
-                correct=0;
-                while(theruns<questions){
-                    ran1=rand.nextInt(maxOp+1-minOp) + minOp;
-                    ran2=rand.nextInt(maxOp+1-minOp) + minOp;
-                    //this is the code that handles the addition questions
-                    if(ops.equals("+")){
-                        System.out.println(ran1 + " + " + ran2);
+          }
+          //this is the code that handles the subtraction questions
+          if(ops.equals("-")){
+            System.out.println(ran1 + " - " + ran2);
 
-                        ans = ran1 + ran2;
+            ans = ran1 - ran2;
+            do{
+              while(!sc.hasNextInt()){
+                sc.next();
+                System.out.println("Error: Invalid answer.");
 
-                        do{
-                            while(!sc.hasNextInt()){
-                                sc.next();
-                                System.out.println("Error: Invalid answer.");
+              }
+            } while(!sc.hasNextInt());
+            useans=sc.nextInt();
+            if(useans==ans){
+              System.out.println("Correct!");
+              correct++;
+            }
+            else{
+              System.out.println("Incorrect");
+            }
+          }
+          //this is the code that handles the multiplication questions
+          if(ops.equals("*")){
+            System.out.println(ran1 + " * " + ran2);
 
-                            }
-                        } while(!sc.hasNextInt());
-                        useans=sc.nextInt();
-                        if(useans==ans){
-                            System.out.println("Correct!");
-                            correct++;
-                        }
-                        else{
-                            System.out.println("Incorrect");
-                        }
+            ans = ran1 * ran2;
+            do{
+              while(!sc.hasNextInt()){
+                sc.next();
+                System.out.println("Error: Invalid answer.");
 
-                    }
-                    //this is the code that handles the subtraction questions
-                    if(ops.equals("-")){
-                        System.out.println(ran1 + " - " + ran2);
+              }
+            }while(!sc.hasNextInt());
+            useans=sc.nextInt();
+            if(useans==ans){
+              System.out.println("Correct!");
+              correct++;
+            }
+            else{
+              System.out.println("Incorrect");
+            }
+          }
+          //this is the code that handles division questions
+          if(ops.equals("/")){
+            ran1=rand.nextInt(maxOp+1-minOp) + minOp;
+            ran2=rand.nextInt(maxOp+1-minOp) + minOp+1;
+            System.out.println(ran1 + " / " + ran2);
 
-                        ans = ran1 - ran2;
-                        do{
-                            while(!sc.hasNextInt()){
-                                sc.next();
-                                System.out.println("Error: Invalid answer.");
+            ans2 = (ran1*+1.00) / (ran2*1.00);
+            do{
+              while(!sc.hasNextDouble()){
+                sc.next();
+                System.out.println("Error: Invalid answer.");
 
-                            }
-                        } while(!sc.hasNextInt());
-                        useans=sc.nextInt();
-                        if(useans==ans){
-                            System.out.println("Correct!");
-                            correct++;
-                        }
-                        else{
-                            System.out.println("Incorrect");
-                        }
-                    }
-                    //this is the code that handles the multiplication questions
-                    if(ops.equals("*")){
-                        System.out.println(ran1 + " * " + ran2);
+              }
+            }while(!sc.hasNextDouble());
+            useans2=sc.nextDouble();
+            if(useans2>(ans2-.01) && useans2<(ans2+.01)){
+              System.out.println("Correct!");
+              correct++;
+            }
+            else{
+              System.out.println("Incorrect");
+            }
+          }
+          //this the code that handles mod questions
+          if(ops.equals("%")){
+            System.out.println(ran1 + " % " + ran2);
 
-                        ans = ran1 * ran2;
-                        do{
-                            while(!sc.hasNextInt()){
-                                sc.next();
-                                System.out.println("Error: Invalid answer.");
+            ans = ran1 % ran2;
+            do{
+              while(!sc.hasNextInt()){
+                sc.next();
+                System.out.println("Error: Invalid answer.");
 
-                            }
-                        }while(!sc.hasNextInt());
-                        useans=sc.nextInt();
-                        if(useans==ans){
-                            System.out.println("Correct!");
-                            correct++;
-                        }
-                        else{
-                            System.out.println("Incorrect");
-                        }
-                    }
-                    //this is the code that handles division questions
-                    if(ops.equals("/")){
-                        ran1=rand.nextInt(maxOp+1-minOp) + minOp;
-                        ran2=rand.nextInt(maxOp+1-minOp) + minOp+1;
-                        System.out.println(ran1 + " / " + ran2);
-
-                        ans2 = (ran1*+1.00) / (ran2*1.00);
-                        do{
-                            while(!sc.hasNextDouble()){
-                                sc.next();
-                                System.out.println("Error: Invalid answer.");
-
-                            }
-                        }while(!sc.hasNextDouble());
-                        useans2=sc.nextDouble();
-                        if(useans2>(ans2-.01) && useans2<(ans2+.01)){
-                            System.out.println("Correct!");
-                            correct++;
-                        }
-                        else{
-                            System.out.println("Incorrect");
-                        }
-                    }
-                    //this the code that handles mod questions
-                    if(ops.equals("%")){
-                        System.out.println(ran1 + " % " + ran2);
-
-                        ans = ran1 % ran2;
-                        do{
-                            while(!sc.hasNextInt()){
-                                sc.next();
-                                System.out.println("Error: Invalid answer.");
-
-                            }
-                        } while(!sc.hasNextInt());
-                        useans=sc.nextInt();
-                        if(useans==ans){
-                            System.out.println("Correct!");
-                            correct++;
-                        }
-                        else{
-                            System.out.println("Incorrect");
-                        }
-                    }
-                    theruns++;
-                    }
-                    System.out.println("You have completed the quiz! Your Score is: " + correct +
-                        " out of " + theruns);
-                    go=0;
-                    }
-                    //If user choses to exit program
-                    if(go==6){
-                    System.out.println("Good Riddance!");
-                    System.exit(6);
-                    }
-                    }
-                    }
-                    }
+              }
+            } while(!sc.hasNextInt());
+            useans=sc.nextInt();
+            if(useans==ans){
+              System.out.println("Correct!");
+              correct++;
+            }
+            else{
+              System.out.println("Incorrect");
+            }
+          }
+          theruns++;
+        }
+        System.out.println("You have completed the quiz! Your Score is: " + correct +
+        " out of " + theruns);
+        go=0;
+      }
+      //If user choses to exit program
+      if(go==6){
+        System.out.println("Good Riddance!");
+        System.exit(6);
+      }
+    }
+  }
+}
